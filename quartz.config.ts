2ea665f5fb224b35671db7eec651439968db22f0
LorenzoPapi <topolino123654@gmail.com>
2024-05-12 11:54:22 +0200

    Aggiustato un pochetto il sito

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "NucleiTematici", "Canovaccio.md"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
