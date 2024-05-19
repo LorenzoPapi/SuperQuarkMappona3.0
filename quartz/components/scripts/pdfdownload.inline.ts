dfc58cfef1e2a28c898f266cad6cbc4389b8fc3d
LorenzoPapi <topolino123654@gmail.com>
2024-05-19 17:37:18 +0200

    Per favore...tiprego....

--- quartz/components/scripts/pdfdownload.inline.ts
+++ quartz/components/scripts/pdfdownload.inline.ts
@@ -6,7 +6,9 @@ async function savePDF(this : HTMLElement) {
   // ma
   // andrà in errore in dev mode
   // cosa sto facendo della mia vita
-  const contentIndexPath = "/SuperQuarkMappona3.0/static/contentIndex.json"
+  const devMode = false
+  const prefix = devMode ? "" : "/SuperQuarkMappona3.0"
+  const contentIndexPath = `${prefix}/static/contentIndex.json`
   const fetchData = fetch(contentIndexPath).then(data => data.json())
   
   const data = await fetchData
@@ -20,7 +22,7 @@ async function savePDF(this : HTMLElement) {
   })
   if (fileName) {
     var link = document.createElement('a')
-    link.href = `/pdfs/${fileName}.pdf`
+    link.href = `${prefix}/pdfs/${fileName}.pdf`
     link.download = fileName.split('/').slice(-1)[0]
     link.click()
   } else console.log('ERRORE file non trovato')
